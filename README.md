7952865784f50e7e675185f00b992ad94a5b59eb
Matthew Pham <mtpham.github@quantamail.net>
2024-02-12 03:27:44 -0600

    submission

---
+++ README.md
@@ -0,0 +1,63 @@
+# Fetch SRE Take-Home Exercise
+
+This is my submission for [Fetch's](https://fetch.com/) site reliability engineer take-home exercise.
+
+
+## HTTP Endpoint Health Checker
+
+### 1. About
+
+This python script ("http_endpoint_checkup.py") reads in a set of HTTP(S) endpoints via a path to a YAML file and checks whether the endpoints are "up" every 15 seconds.
+
+Endpoints are considered "up" if they give a response within 500 [ms] and a 2xx status code (i.e. between 200-299 inclusive)
+
+This script utilizes [aiohttp](https://docs.aiohttp.org/en/stable/index.html) to perform asynchronous http requests.
+
+
+### 2. Usage
+
+This script is written in python and was tested with python 3.9-3.12.
+***Running this script requires python >= 3.9 and pip for installing dependencies***.
+
+
+#### 2.1 Dependencies
+
+1. [aiohttp](https://docs.aiohttp.org/en/stable/index.html)
+2. [pyyaml](https://pyyaml.org/)
+
+
+#### 2.2 Install/Setup
+
+First clone this repository:
+
+```
+git clone https://github.com/mtpham99/fetch-sre-takehome.git
+cd fetch-sre-takehome
+```
+
+Next, install the dependencies. Easiest way to install the dependencies is using [pip](https://pypi.org/project/pip/). It is also recommended to install into a virtual environment:
+
+```
+# assuming python3 is >= 3.9
+python3 -m venv /path/to/virtual-environment  # create virtual environment
+source /path/to/virtual-environment  # activate virtual environment
+pip install . # install dependencies (aiohttp and pyyaml)
+```
+
+#### 2.3 Running
+
+See "help":
+```
+python3 http_endpoint_checkup.py --help
+```
+
+Run the sample inputs:
+```
+python3 http_endpoint_checkup.py sample.yaml
+```
+
+Run your own input file:
+```
+python3 http_endpoint_checkup.py /path/to/your/input/yaml/file
+```
+
---
+++ http_endpoint_checkup.py
@@ -0,0 +1,171 @@
+import argparse
+import asyncio
+import enum
+import logging
+import os
+import sys
+from typing import Optional, TypedDict
+from urllib.parse import urlparse
+
+import aiohttp
+import yaml
+
+CHECKUP_FREQUENCY_S = 15.0  # number of seconds between each endpoint checkup
+ENDPOINT_UP_TIMELIMIT = 0.5  # respone timelimit to be considered up
+
+
+class EndpointStatus(enum.Enum):
+    UP = "up"
+    DOWN = "down"
+
+
+class HTTPEndpointData(TypedDict):
+    name: str
+    url: str
+    method: str  # defaults to "GET"
+    headers: Optional[dict[str, str]]  # key:value pairs (or None)
+    body: Optional[str]  # JSON encoded string (or None)
+
+
+def url_to_domain(url: str) -> str:
+    return urlparse(url).netloc  # scheme://netloc/path...
+
+
+def parse_input(configfile: str) -> list[HTTPEndpointData]:
+    endpoints: list[HTTPEndpointData] = []
+    with open(configfile, "r") as f:  # pylint: disable=unspecified-encoding
+        for ep in yaml.safe_load(f):
+            endpoints.append(
+                HTTPEndpointData(
+                    {
+                        "name": ep["name"],
+                        "url": ep["url"],
+                        "method": ep.get(
+                            "method", "GET"  # defaults to "GET" method)
+                        ),
+                        "headers": ep.get("headers", None),
+                        "body": ep.get("body", None),
+                    }
+                )
+            )
+    return endpoints
+
+
+async def check_endpoint(
+    endpoint: HTTPEndpointData,
+    session: aiohttp.ClientSession,
+) -> tuple[str, EndpointStatus]:
+
+    url = endpoint["url"]
+    method = endpoint["method"]
+    headers = endpoint["headers"]
+    body = endpoint["body"]
+
+    status: EndpointStatus
+    try:
+        async with session.request(
+            method=method,
+            url=url,
+            headers=headers,
+            json=body,
+            timeout=aiohttp.ClientTimeout(total=ENDPOINT_UP_TIMELIMIT),
+        ) as resp:
+            # endpoint is up if status code [200, 299]
+            if 200 <= resp.status <= 299:
+                status = EndpointStatus.UP
+            else:
+                status = EndpointStatus.DOWN
+
+    # endpoint is down in case of timeout
+    except asyncio.TimeoutError:
+        status = EndpointStatus.DOWN
+
+    return (url, status)
+
+
+async def mainloop(endpoints: list[HTTPEndpointData]) -> None:
+    # create aiohttp client session
+    connector = aiohttp.TCPConnector(limit=None)  # type: ignore [arg-type]
+    timeout = aiohttp.ClientTimeout(total=None)
+    async with aiohttp.ClientSession(
+        connector=connector, timeout=timeout
+    ) as session:
+
+        # keep track of up counts/availability
+        domains_up_counts = {
+            url_to_domain(ep["url"]): {"ups": 0, "total": 0}
+            for ep in endpoints
+        }
+
+        # main loop
+        while True:
+
+            # check endpoints
+            coros = [check_endpoint(ep, session) for ep in endpoints]
+            for task in asyncio.as_completed(coros):
+
+                # get url, url/endpoint's status, and domain
+                url, status = await task
+                domain = url_to_domain(url)
+
+                # update counts
+                domains_up_counts[domain]["total"] += 1
+                if status is EndpointStatus.UP:
+                    domains_up_counts[domain]["ups"] += 1
+
+            # print/log counts
+            for domain, counts in domains_up_counts.items():
+                ups = counts["ups"]
+                total = counts["total"]
+                logging.log(
+                    level=logging.INFO,
+                    msg=(
+                        f"{domain} has {round(100.0 * ups / total)}%"
+                        " availability percentage"
+                    ),
+                )
+
+            # sleep until next iteration
+            await asyncio.sleep(CHECKUP_FREQUENCY_S)
+
+
+if __name__ == "__main__":
+
+    # logging (stdout)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    # parse arg(s)
+    parser = argparse.ArgumentParser(
+        prog="HTTP Endpoint Checkup",
+        description=(
+            "Script to parse a yaml file with a set of endpoints and "
+            "check whether the endpoints are up or down every 15 sesconds."
+        ),
+    )
+    parser.add_argument(
+        "configfile",
+        help="Path to yaml config file containing set of http(s) endpoints",
+        type=str,
+    )
+    args = parser.parse_args()
+
+    # check input
+    if not os.path.isfile(args.configfile):
+        raise ValueError(
+            f'Config file path ("{args.configfile}") is not a file. Aborting.'
+        )
+
+    # parse input
+    endpoints_list: list[HTTPEndpointData] = parse_input(args.configfile)
+
+    # run main until user interrupt
+    try:
+        asyncio.run(mainloop(endpoints_list))
+
+    # user interrupt
+    except KeyboardInterrupt:
+        pass
